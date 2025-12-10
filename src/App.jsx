1a5fc101bd8a4737e50c605d733a12c79c8b35a5
Bryan <bryanribero@hotmail.com>
2025-12-10 04:16:52 -0300

    Practicando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Saludo } from './components/Saludo.jsx'
-import Evento from './components/Evento.jsx'
-import Form from './components/Form.jsx'
-import Contador from './components/Contador.jsx'
-import ListaTareas from './components/ListaTareas.jsx'
-import FormularioUno from './components/Formulario.jsx'
-import UseEffect from './components/UseEffect.jsx'
-import Fetch from './components/Fetch.jsx'
-import TodoList from './components/TodoList.jsx'
-import Tareas from './components/Tareas.jsx'
-import FetchNuevo from './components/FetchNuevo.jsx'
-import SendData from './components/SendData.jsx'
-import ContadorPadre from './components/ContadorPadre.jsx'
+import Home from './pages/Home.jsx'
+import Contador from './pages/Contador.jsx'
 
 export default function App() {
   return (
     <>
-      <main>
-        <ContadorPadre />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contador" element={<Contador />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Contador.jsx
@@ -0,0 +1,10 @@
+import ContadorPadre from '../components/ContadorPadre'
+
+export default function Contador() {
+  return (
+    <div>
+      <h1>Contador</h1>
+      <ContadorPadre />
+    </div>
+  )
+}
