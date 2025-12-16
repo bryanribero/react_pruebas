9d02282427fdbb4f25b4df126640c6a71c3d07e4
Bryan <bryanribero@hotmail.com>
2025-12-15 22:50:31 -0300

    Practica con context api para el manejo de estado global

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import './App.css'
 import Home from './pages/Home.jsx'
 import Productos from './pages/Productos.jsx'
 import ProductoInfo from './dynamicPages/ProductoInfo.jsx'
+import ThemeProvider from './Context/ThemeProvider.jsx'
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos />} />
-        <Route path="/productos/:id" element={<ProductoInfo />} />
-      </Routes>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/productos/:id" element={<ProductoInfo />} />
+        </Routes>
+      </ThemeProvider>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
+import useTheme from '../Context/useTheme'
 
 export default function Home() {
+  const { theme, toggleTheme } = useTheme()
+
   return (
     <div>
       <h1>Bienvenido al home</h1>
       <Link to={'/productos'}>Ir a productos</Link>
+      <br />
+      <br />
+      <button onClick={toggleTheme}>Cambiar thema</button>
+      <p>{theme}</p>
     </div>
   )
 }
